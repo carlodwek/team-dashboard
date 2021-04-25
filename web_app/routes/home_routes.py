955f6570059bcc958e760736aae0b11e8989f78e
carlodwek <carlodwek@gmail.com>
2021-04-25 17:07:58 -0400

    Initial Set Up
    .travis.yml, conftest and test folder, Procfile, requirements, web_app folder with init, routes folder with home routes, templates folder with bootstrap, home and about, app folder,

---
+++ web_app/routes/home_routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, request, render_template
+
+home_routes = Blueprint("home_routes", __name__)
+
+@home_routes.route("/")
+@home_routes.route("/home")
+def index():
+    print("HOME...")
+    return render_template("home.html")
+
+@home_routes.route("/about")
+def about():
+    print("ABOUT...")
+    return render_template("about.html")
