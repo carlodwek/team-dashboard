d4754b08acebeeaa10aee1aa43ad8c6fc2164915
carlodwek <carlodwek@gmail.com>
2021-04-26 00:10:29 -0400

    added the basic API functionality and created a quick html mockup to test it

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # team-dashboard
+
+##Setup
+
+##Using the API
+https://fly.sportsdata.io/v3/soccer/scores/json/Competitions?key=960a5f575afb4c7887b0262f17783434
+
+find matching competition id / name
+find right season, find right round
+find SeasonId 128
+find RoundId 645
+
+using seasonid
+https://fly.sportsdata.io/v3/soccer/scores/json/SeasonTeams/128?key=960a5f575afb4c7887b0262f17783434
+
+find matching team id / name
+logo url
+
+
+using round id
+https://fly.sportsdata.io/v3/soccer/scores/json/Standings/645?key=960a5f575afb4c7887b0262f17783434
+
+get Standings to display
+
+using round id
+https://fly.sportsdata.io/v3/soccer/scores/json/Schedule/645?key=960a5f575afb4c7887b0262f17783434
+
+sort schedule by teamid
+get the season's matches / find latest match
---
+++ app/sport.py
@@ -0,0 +1,92 @@
+import os
+from dotenv import load_dotenv
+import json
+import requests
+import pandas as pd
+from datetime import datetime
+
+# from app import APP_ENV
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+LEAGUE = os.getenv("LEAGUE", default="Serie A")
+TEAM = os.getenv("TEAM", default="Inter")
+APP_ENV = os.getenv("APP_ENV", default="development")
+
+def SetParams():
+    if APP_ENV == "development":
+        league = input("PLEASE INPUT A LEAGUE: ")
+        team = input("PLEASE INPUT A TEAM: ")
+    else:
+        league = LEAGUE
+        team = TEAM
+    return league, team
+
+def GetIds(league, team):
+    response = requests.get(f"https://fly.sportsdata.io/v3/soccer/scores/json/Competitions?key={API_KEY}")
+    if response.status_code != 200:
+        return None, None, None, None
+    parsed_response = json.loads(response.text)
+    tempList = [i['Seasons'] for i in parsed_response if i['Name'] == league][0]
+    tempList = [i for i in tempList if i['CurrentSeason'] == True][0]
+    SeasonId = tempList['SeasonId']
+    print(SeasonId)
+    RoundId = [i['RoundId'] for i in tempList['Rounds'] if i['CurrentRound'] == True][0]
+    print(RoundId)
+
+    response = requests.get(f"https://fly.sportsdata.io/v3/soccer/scores/json/SeasonTeams/{SeasonId}?key={API_KEY}")
+    if response.status_code != 200:
+        return None, None, None, None
+    parsed_response = json.loads(response.text)
+    TeamList = [i for i in parsed_response if i['TeamName'] == team][0]
+    TeamId = TeamList['TeamId']
+    LogoUrl = TeamList['Team']['WikipediaLogoUrl']
+    print(TeamId)
+    print(LogoUrl)
+
+    return SeasonId, RoundId, TeamId, LogoUrl
+
+def GetStandings(RoundId):
+    response = requests.get(f"https://fly.sportsdata.io/v3/soccer/scores/json/Standings/{RoundId}?key={API_KEY}")
+    if response.status_code != 200:
+        return None
+    parsed_response = json.loads(response.text)
+    standings = [i for i in parsed_response if i['Scope'] == "Total"]
+    return standings
+
+def GetSchedule(RoundId, TeamId):
+    response = requests.get(f"https://fly.sportsdata.io/v3/soccer/scores/json/Schedule/{RoundId}?key={API_KEY}")
+    if response.status_code != 200:
+        return None
+    parsed_response = json.loads(response.text)
+    schedule = [i for i in parsed_response if i['AwayTeamId'] == TeamId or i['HomeTeamId'] == TeamId]
+    return schedule
+
+if __name__ == "__main__":
+
+    print(f"RUNNING THE LIVE SCORES APP IN {APP_ENV.upper()} MODE...")
+
+    league, team = SetParams()
+    print("LEAGUE:", league)
+    print("TEAM:", team)
+
+    SeasonId, RoundId, TeamId, LogoUrl = GetIds(league, team)
+    if not SeasonId or not RoundId or not TeamId or not LogoUrl:
+        print("INVALID PARAMS. PLEASE CHECK YOUR INPUTS AND TRY AGAIN!")
+        exit()
+    print(SeasonId, RoundId, TeamId, LogoUrl)
+
+    standings = GetStandings(RoundId)
+    for i in standings:
+        print(i["Name"],i["Points"])
+    schedule = GetSchedule(RoundId, TeamId)
+    for i in schedule:
+        print(i["DateTime"],"-", i["HomeTeamName"], str(i["HomeTeamScore"])+"-"+str(i["AwayTeamScore"]), i["AwayTeamName"])
+    now = datetime.now()
+    for i in schedule:
+        x = datetime.strptime(i["DateTime"], '%Y-%m-%dT%H:%M:%S')
+        if x > now:
+            next = i
+            break
+    print(next["DateTime"], "-", next["HomeTeamName"], str(next["HomeTeamScore"])+"-"+str(next["AwayTeamScore"]), next["AwayTeamName"])
